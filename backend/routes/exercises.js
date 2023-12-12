b963d13e992758bdd2b09299d841c0d77907b1f6
vamsidhar32 <Vamsidhar.Paluchani@iiitb.ac.in>
2023-12-13 00:40:49 +0530

    ansible

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -1,13 +1,90 @@
+
+
+
+// const router = require('express').Router();
+// let Exercise = require('../models/exercise.model');
+
+// router.route('/').get((req, res) => {
+//   Exercise.find()
+//     .then(exercises => res.json(exercises))
+//     .catch(err => res.status(400).json('Error: ' + err));
+// });
+
+// router.route('/add').post((req, res) => {
+//   const username = req.body.username;
+//   const description = req.body.description;
+//   const duration = Number(req.body.duration);
+//   const date = Date.parse(req.body.date);
+
+//   const newExercise = new Exercise({
+//     username,
+//     description,
+//     duration,
+//     date,
+//   }); data, duration username, 
+
+//   newExercise.save()
+//   .then(() => res.json('Exercise added!'))
+//   .catch(err => res.status(400).json('Error: ' + err));
+// });
+// router.route('/:id) .get ((req,res))
+// router.route('/:id').get((req, res) => {
+//   Exercise.findById(req.params.id)
+//     .then(exercise => res.json(exercise))
+//     .catch(err => res.status(400).json('Error: ' + err));
+// });
+
+// router.route('/:id').delete((req, res) => {
+//   Exercise.findByIdAndDelete(req.params.id)
+//     .then(() => res.json('Exercise deleted.'))
+//     .catch(err => res.status(400).json('Error: ' + err));
+// });
+
+// router.route('/update/:id').post((req, res) => {
+//   Exercise.findById(req.params.id)
+//     .then(exercise => {
+//       exercise.username = req.body.username;
+//       exercise.description = req.body.description;
+//       exercise.duration = Number(req.body.duration);
+//       exercise.date = Date.parse(req.body.date);
+
+//       exercise.save()
+//         .then(() => res.json('Exercise updated!'))
+//         .catch(err => res.status(400).json('Error: ' + err));
+//     })
+//     .catch(err => res.status(400).json('Error: ' + err));
+// });
+
+// module.exports = router;
+
+
+
+
+//const logger=require('../logger/logging')
+
+
+
 const router = require('express').Router();
+//const logger = require('your-logger-library'); // Import or configure your logger library
+
 let Exercise = require('../models/exercise.model');
+const logger=require('../logger/logging')
+
+
+
 
 router.route('/').get((req, res) => {
+  logger.info('GET /exercises');
   Exercise.find()
     .then(exercises => res.json(exercises))
-    .catch(err => res.status(400).json('Error: ' + err));
+    .catch(err => {
+      logger.error('Error fetching exercises:', err);
+      res.status(400).json('Error: ' + err);
+    });
 });
 
 router.route('/add').post((req, res) => {
+  logger.info('POST /exercises/add');
   const username = req.body.username;
   const description = req.body.description;
   const duration = Number(req.body.duration);
@@ -21,24 +98,48 @@ router.route('/add').post((req, res) => {
   });
 
   newExercise.save()
-  .then(() => res.json('Exercise added!'))
-  .catch(err => res.status(400).json('Error: ' + err));
+    .then(() => {
+      logger.info('Exercise added!');
+      res.json('Exercise added!');
+    })
+    .catch(err => {
+      logger.error('Error adding exercise:', err);
+      res.status(400).json('Error: ' + err);
+    });
 });
 
 router.route('/:id').get((req, res) => {
-  Exercise.findById(req.params.id)
-    .then(exercise => res.json(exercise))
-    .catch(err => res.status(400).json('Error: ' + err));
+  const exerciseId = req.params.id;
+  logger.info(`GET /exercises/${exerciseId}`);
+  Exercise.findById(exerciseId)
+    .then(exercise => {
+      logger.info('Exercise found:', exercise);
+      res.json(exercise);
+    })
+    .catch(err => {
+      logger.error('Error fetching exercise:', err);
+      res.status(400).json('Error: ' + err);
+    });
 });
 
 router.route('/:id').delete((req, res) => {
-  Exercise.findByIdAndDelete(req.params.id)
-    .then(() => res.json('Exercise deleted.'))
-    .catch(err => res.status(400).json('Error: ' + err));
+  const exerciseId = req.params.id;
+  logger.info(`DELETE /exercises/${exerciseId}`);
+  Exercise.findByIdAndDelete(exerciseId)
+    .then(() => {
+      logger.info('Exercise deleted.');
+      res.json('Exercise deleted.');
+    })
+    .catch(err => {
+      logger.error('Error deleting exercise:', err);
+      res.status(400).json('Error: ' + err);
+    });
 });
 
 router.route('/update/:id').post((req, res) => {
-  Exercise.findById(req.params.id)
+  const exerciseId = req.params.id;
+  logger.info(`POST /exercises/update/${exerciseId}`);
+  Exercise.findById(exerciseId)
     .then(exercise => {
       exercise.username = req.body.username;
       exercise.description = req.body.description;
@@ -46,10 +147,20 @@ router.route('/update/:id').post((req, res) => {
       exercise.date = Date.parse(req.body.date);
 
       exercise.save()
-        .then(() => res.json('Exercise updated!'))
-        .catch(err => res.status(400).json('Error: ' + err));
+        .then(() => {
+          logger.info('Exercise updated!');
+          res.json('Exercise updated!');
+        })
+        .catch(err => {
+          logger.error('Error updating exercise:', err);
+          res.status(400).json('Error: ' + err);
+        });
     })
-    .catch(err => res.status(400).json('Error: ' + err));
+    .catch(err => {
+      logger.error('Error finding exercise for update:', err);
+      res.status(400).json('Error: ' + err);
+    });
 });
 
 module.exports = router;
+
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,21 +1,62 @@
+// const router = require('express').Router();
+// let User = require('../models/user.model');
+
+// router.route('/').get((req, res) => {
+//   User.find()
+//     .then(users => res.json(users))
+//     .catch(err => res.status(400).json('Error: ' + err));
+// });
+
+// router.route('/add').post((req, res) => {
+//   const username = req.body.username;
+  
+//   const newUser = new User({username});
+  
+//   newUser.save()
+//     .then(() => res.json('User added!'))
+//     .catch(err => res.status(400).json('Error: ' + err));
+//   console.log(res)
+// });
+
+// module.exports = router;
+
+
 const router = require('express').Router();
+//const logger = require('your-logger-library'); // Import or configure your logger library
 let User = require('../models/user.model');
+const logger=require('../logger/logging')
+
+
 
 router.route('/').get((req, res) => {
+  logger.info('GET /users');
   User.find()
-    .then(users => res.json(users))
-    .catch(err => res.status(400).json('Error: ' + err));
+    .then(users => {
+      logger.info('Users found:', users);
+      res.json(users);
+    })
+    .catch(err => {
+      logger.error('Error fetching users:', err);
+      res.status(400).json('Error: ' + err);
+    });
 });
 
 router.route('/add').post((req, res) => {
   const username = req.body.username;
+  logger.info(`POST /users/add - Adding user: ${username}`);
   
   const newUser = new User({username});
   
   newUser.save()
-    .then(() => res.json('User added!'))
-    .catch(err => res.status(400).json('Error: ' + err));
-  console.log(res)
+    .then(() => {
+      logger.info('User added!');
+      res.json('User added!');
+    })
+    .catch(err => {
+      logger.error('Error adding user:', err);
+      res.status(400).json('Error: ' + err);
+    });
 });
 
 module.exports = router;
+
