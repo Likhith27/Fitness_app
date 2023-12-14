8ed1820fade866a9d5617a89a60f6a89c186bb04
Likhith27 <Likhith.Kumar@iiitb.ac.in>
2023-12-15 01:12:13 +0530

    added docker compse

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,36 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv/config");
 const mongoose = require("mongoose");
+const bodyParser = require('body-parser')
+const corsOptions = {
+    origin:'http://localhost:3000',
+    credentials: true,
+    optionSuccessStatus: 200
+}
 
 const app = express();
+app.use(cors(corsOptions));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json());
 const port = process.env.PORT || 5000;
 
-app.use(cors());
-app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello to Fitness Tracker API");
 });
+app.use(cors({
+  origin: '*',
+  methods: '*',   
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin: *');
+  res.header('Access-Control-Allow-Methods: *');
+  res.header('Access-Control-Allow-Headers: *');
+  next();
+});
+
 
 mongoose.connect(
   process.env.MONGO_URI,
